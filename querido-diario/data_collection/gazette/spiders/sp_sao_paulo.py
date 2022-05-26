c6db06b75a9899d8f73d70f15abae7d913b7b157
VinicusGB <viniciusgomesb@gmail.com>
2022-05-26 14:51:31 -0300

    feat: novas listagens

--- querido-diario/data_collection/gazette/spiders/sp_sao_paulo.py
+++ querido-diario/data_collection/gazette/spiders/sp_sao_paulo.py
@@ -16,6 +16,7 @@ class SpSaoPauloSpider(BaseGazetteSpider):
     BASE_URL = "http://diariooficial.imprensaoficial.com.br"
     allowed_domains = ["diariooficial.imprensaoficial.com.br"]
     name = "sp_sao_paulo"
+    #start_date = date(2022, 5, 1)
     start_date = date.today()
 
     def start_requests(self):
